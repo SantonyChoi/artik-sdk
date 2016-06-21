8fa6ccbbd28e34915578a1262c792fc0abdadc46
root <root@localhost.localdomain>
2016-06-21 09:45:38 -0400

    Websocket: Clean up     removed miscellaneous informations

--- test/websocket-test.js
+++ test/websocket-test.js
@@ -4,13 +4,13 @@ var artik = require('../lib/artik-sdk');
 var auth_token = '';
 var device_id = '';
 //var host = "s-api.samsungsami.io"; //for sdr
-var host = "api.samsungsami.io"; //normal websocket
+var host = "api.artik.cloud"; 
 var uri = "/v1.1/websocket?ack=true";
 var port = 443;
 var ssl_connection = 2;
 var use_se = false;
-var register_message = '{"sdid":"f8b8e08f08d24e278bc574f7361d207f","Authorization":"bearer 8d7feba956ae41788b2b2295a8a7d132","type":"register"}';
-var test_send_message = '{"data": {"actions": [{"name": "setOn"}]},"ddid":"f8b8e08f08d24e278bc574f7361d207f","sdid":"f8b8e08f08d24e278bc574f7361d207f","type":"message"}'
+var register_message = '{"sdid":"","Authorization":"","type":"register"}';
+var test_send_message = ''
 
 var conn = new websocket(host, uri, port, ssl_connection, use_se);
 
